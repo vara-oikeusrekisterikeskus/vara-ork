44ae74a0437f57624e4beabbd721294ac8de7e3b
Alexander Tallqvist <alexander.tallqvist@siili.com>
2025-01-28 13:39:18 +0200

    Updated page config.

--- app/lib/getPageConfig.js
+++ app/lib/getPageConfig.js
@@ -27,6 +27,58 @@ const pageConfig = {
       primary: "#0A5a55",
       secondary: "#0A5a55",
     },
+  },
+  oikeus: {
+    fi: {
+      title: "Oikeus.fi varasivusto",
+      description: "Oikeus.fi varasivusto.",
+      name: "Oikeus.fi",
+      logo: "/logos/oikeuspalveluvirasto_header_fi.svg",
+      logoHeight: "48px",
+      logoFooter: "/logos/oikeuspalveluvirasto_footer_fi.svg",
+      logoFooterHeight: "48px",
+    },
+    en: {
+      title: "Backup site for Oikeus.fi",
+      description: "Backup site for Oikeus.fi.",
+      name: "Oikeus.fi",
+      logo: "/logos/oikeuspalveluvirasto_header_en.svg",
+      logoHeight: "48px",
+      logoFooter: "/logos/oikeuspalveluvirasto_footer_en.svg",
+      logoFooterHeight: "48px",
+    },
+    favicon: "/favicons/oikeuspalveluvirasto.png",
+    contentId: "cm6geenkv21uy07lf434hvgec",
+    colors: {
+      primary: "#7F261E",
+      secondary: "#7F261E",
+    },
+  },
+  konkurssiasiamies: {
+    fi: {
+      title: "Konkurssiasiamiehen toimiston varasivusto",
+      description: "Konkurssiasiamiehen toimiston varasivusto.",
+      name: "Konkurssiasiamies",
+      logo: "/logos/oikeuspalveluvirasto_header_fi.svg",
+      logoHeight: "48px",
+      logoFooter: "/logos/oikeuspalveluvirasto_footer_fi.svg",
+      logoFooterHeight: "48px",
+    },
+    en: {
+      title: "Backup site for The Office of Bankruptcy Ombudsman",
+      description: "Backup site for The Office of Bankruptcy Ombudsman.",
+      name: "The Office of Bankruptcy Ombudsman",
+      logo: "/logos/oikeuspalveluvirasto_header_en.svg",
+      logoHeight: "48px",
+      logoFooter: "/logos/oikeuspalveluvirasto_footer_en.svg",
+      logoFooterHeight: "48px",
+    },
+    favicon: "/favicons/oikeuspalveluvirasto.png",
+    contentId: "cm6geg7ay22pu07mlftug6tlq",
+    colors: {
+      primary: "#D64309",
+      secondary: "#082B42",
+    },
   }
 }
 
